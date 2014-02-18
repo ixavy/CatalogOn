01b1680ee31898d1d64be68f020de4fa1146784c
eduardo fulgencio <eduardofulgencio@MacBook-Pro-de-eduardo.local>
2014-02-18 18:10:51 +0100

    Initial Commit

---
+++ CatalogOn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CatalogOn
+//
+//  Created by eduardo fulgencio on 18/02/14.
+//  Copyright (c) 2014 Eduardo Fulgencio Comendeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CatalogOnTests/CatalogOnTests.m
@@ -0,0 +1,34 @@
+//
+//  CatalogOnTests.m
+//  CatalogOnTests
+//
+//  Created by eduardo fulgencio on 18/02/14.
+//  Copyright (c) 2014 Eduardo Fulgencio Comendeiro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CatalogOnTests : XCTestCase
+
+@end
+
+@implementation CatalogOnTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
